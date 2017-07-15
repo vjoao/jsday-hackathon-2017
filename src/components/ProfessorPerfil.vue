3c96c215c33a72e021a1c5cf713059554da1b08c
Jones Dias <diasjones07@gmail.com>
2017-07-15 15:30:21 -0300

    add professor perfil

--- src/components/ProfessorPerfil.vue
+++ src/components/ProfessorPerfil.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="container">
         <Navigation></Navigation>
+
+        <md-layout md-gutter>
+        <md-layout md-column md-gutter>
+            <md-layout md-flex="20"></md-layout>
+            <md-layout></md-layout>
+        </md-layout>
+
+        <md-layout md-column md-gutter>
+            <md-layout md-flex="50"></md-layout>
+            <md-layout></md-layout>
+            <md-layout></md-layout>
+        </md-layout>
+        </md-layout>
     </div>
 </template>
 

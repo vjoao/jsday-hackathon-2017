ac0cbb8e39389b02d7c8fa0fb2b8cd7155aea6a0
Victor Nascimento <victormota15@gmail.com>
2017-07-15 15:14:32 -0300

    Add professors to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,238 +1,67 @@
 <template>
 	<div class="container">
 		<!-- navigation -->
-			<Navigation />
-
-			<div class="intro-message">
-							<p class="search-message">Buscar <span class="blue">professores</span></p>
-							<p class="find-new-teacher">Encontre os melhores professores para seus alunos</p>
-			</div>
-
-			<div class="search-form-home">
-					<input type="text" class="search-home" name="search" placeholder="Digite uma matéria"/>
-			</div>
-
-				<md-layout md-row>
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-					
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-					
-					<md-layout md-flex="25" class="cards-teacher">
-						<md-card>
-							<md-card-header>
-								<div class="md-title">John Doe</div>
-								<div class="md-subhead">Matemática, Física, Química</div>
-							</md-card-header>
-
-							<md-card-expand>
-								<md-card-actions>
-									<md-button>Ver Perfil</md-button>
-									<md-button>Chat</md-button>
-									<span style="flex: 1"></span>
-									<md-button class="md-icon-button" md-expand-trigger>
-										<md-icon>keyboard_arrow_down</md-icon>
-									</md-button>
-								</md-card-actions>
-
-								<md-card-content>
-									<p>Contato: 75 9 9999-0000</p>
-									<p>Redes sociais: <a href="#">Twitter</a>, <a href="#">Facebook</a></p>
-								</md-card-content>
-							</md-card-expand>
-						</md-card>
-					</md-layout>
-
-				</md-layout>
+		<Navigation />
+	
+		<div class="intro-message">
+			<p class="search-message">Buscar
+				<span class="blue">professores</span>
+			</p>
+			<p class="find-new-teacher">Encontre os melhores professores para seus alunos</p>
+		</div>
+	
+		<div class="search-form-home">
+			<input type="text" class="search-home" name="search" placeholder="Digite uma matéria" />
+		</div>
+	
+		<md-layout md-row>
+			<md-layout md-flex="25" class="cards-teacher" v-for="professor in professors">
+				<md-card>
+					<md-card-header>
+						<div class="md-title">{{ professor.name }}</div>
+						<div class="md-subhead">Matemática, Física, Química</div>
+					</md-card-header>
+	
+					<md-card-expand>
+						<md-card-actions>
+							<md-button>Ver Perfil</md-button>
+							<md-button>Chat</md-button>
+							<span style="flex: 1"></span>
+							<md-button class="md-icon-button" md-expand-trigger>
+								<md-icon>keyboard_arrow_down</md-icon>
+							</md-button>
+						</md-card-actions>
+	
+						<md-card-content>
+							<p>Contato: {{ professor.phone }}</p>
+							<p>Redes sociais:
+								<a href="#">Twitter</a>,
+								<a href="#">Facebook</a>
+							</p>
+						</md-card-content>
+					</md-card-expand>
+				</md-card>
+			</md-layout>
+	
+		</md-layout>
 	</div>
 </template>
 
 <script>
-	import firebase from 'firebase'
-	import db from '../db';
-	import Navigation from './Navigation.vue'
+import firebase from 'firebase'
+import db from '../db';
+import Navigation from './Navigation.vue'
 
-	export default {
-		data() {
-			return {
-				user: {},
-				db: {}
-			}
-		},
-		components: {
-			Navigation
+export default {
+	components: {
+		Navigation
+	},
+	firebase: {
+		professors: {
+			source: db.ref('/professors')
 		}
 	}
+}
 </script>
 
 <style>

594da9b89aa28f8bc9e59c5c1f7c4fbab0fbb62f
Victor Nascimento <victormota15@gmail.com>
2017-07-15 16:30:50 -0300

    Add fuzzy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -67,7 +67,7 @@ export default {
 		filteredProfessors () {
 			const options = {
 				shouldSort: true,
-				threshold: 0.6,
+				threshold: 0.3,
 				location: 0,
 				distance: 100,
 				maxPatternLength: 32,

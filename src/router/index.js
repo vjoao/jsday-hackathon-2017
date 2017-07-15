f75f1b5408cab5776966e24e57148406e1cfa13a
Victor Nascimento <victormota15@gmail.com>
2017-07-15 13:56:13 -0300

    Add login

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
-
+import firebase from 'firebase';
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/home',
+      name: 'Hello',
+      component: Hello
     }
   ]
 })
+
+export default router
